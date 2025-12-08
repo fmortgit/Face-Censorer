47601e03c096ae1e2175b1a807e2079723e0a988
bogatini <86053070+Bogatini@users.noreply.github.com>
2025-12-08 03:52:29 +0000

    updated to check for multiple faces

--- FaceCensorer.py
+++ FaceCensorer.py
@@ -10,26 +10,28 @@ class FaceCensorer():
 
     def censorImage(self, image):
         grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        face = self.faceClassifier.detectMultiScale(grayImage)
+        faceList = self.faceClassifier.detectMultiScale(grayImage)
 
-        if len(face) != 0:
+        if len(faceList) != 0:
 
-            # face is an ndarray with [[xPos, yPos, width, height]]
-            values = face[0]
+            for face in faceList:
 
-            x, y, width, height = values[0], values[1], values[2], values[3]
+                # face is an ndarray with [[xPos, yPos, width, height]]
+                values = face
+
+                x, y, width, height = values[0], values[1], values[2], values[3]
 
 
-            for i in range(x, x+width, self.squareSize):
-                for j in range(y, y+height, self.squareSize):
+                for i in range(x, x+width, self.squareSize):
+                    for j in range(y, y+height, self.squareSize):
 
-                    roi = image[j:j+self.squareSize, i:i+self.squareSize]
+                        roi = image[j:j+self.squareSize, i:i+self.squareSize]
 
-                    r = np.mean(roi[:,:,0])
-                    g = np.mean(roi[:,:,1])
-                    b = np.mean(roi[:,:,2])
-                    avgColour = (r,g,b)
+                        r = np.mean(roi[:,:,0])
+                        g = np.mean(roi[:,:,1])
+                        b = np.mean(roi[:,:,2])
+                        avgColour = (r,g,b)
 
-                    cv2.rectangle(image, (i,j), (i+self.squareSize, j+self.squareSize), avgColour, -1)
+                        cv2.rectangle(image, (i,j), (i+self.squareSize, j+self.squareSize), avgColour, -1)
 
         return image
